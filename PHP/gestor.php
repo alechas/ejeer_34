43e4994b4796b3da06ffaab486bd0b7a13943181
alexischas@hotmail.com <alexischas@hotmail.com>
2016-09-19 21:45:05 -0300

    Correcto

--- PHP/gestor.php
+++ PHP/gestor.php
@@ -7,6 +7,8 @@ class Gestor
 	{
 		if (isset($_FILES["file"])) 
 		{
+			$cant_ok=0;
+			$cont=0;
 			//obtengo la direccion de mi archivo a levantar
 			$path=$_FILES["file"]["tmp_name"];
 
@@ -15,22 +17,41 @@ class Gestor
 
 			//recorro el archivo
 			while (!FeoF($archivo)) 
-			{echo "<br>";
+			{	
+				$cont=0;
 				$renglon = fgets($archivo); //lectura linea a linea
 
 				$mi_array=str_split($renglon);
 				//var_dump($mi_array);
-				//foreach ($mi_array as $carac) 
-				//{
-					echo $renglon;
-					//echo $carac;
-				//}
-
+				foreach ($mi_array as $carac) 
+				{//echo "<br>carac";
+					//var_dump($carac);
+					if ($carac==" ")
+					{				//pregunto si es igual a la cantidad de caracteres que paso como param	
+						if ($cant_carac == $cont && $cant_carac!=0) 
+						{
+							$cant_ok++;//sumo 1 a las coincidencias
+						}
+						if ($cant_carac == 0 && 4 < $cont) 
+						{
+							$cant_ok++;
+						}
+						$cont = 0;
+					}
+					else
+					{
+						$cont++; //si no es la finalización de una palabra, le sumo 1 al contador de la palabra
+					}
+				}
+				
+				if ($cant_carac == $cont) 
+				{
+					$cant_ok++;
+				}
+						
 			}
-			echo "<br>";
-			echo "fin del archivo";
 			fclose($archivo);
-
+			return $cant_ok;
 		}
 	}
 
--- nexo.php
+++ nexo.php
@@ -8,6 +8,11 @@ $palabras3= Gestor::LeerCaracteres(3);
 $palabras4= Gestor::LeerCaracteres(4);
 $palabras0= Gestor::LeerCaracteres(0);
 
+echo "Cantidad de palabras de 1 caracter $palabras1 <br>";
+echo "Cantidad de palabras de 2 caracter $palabras2 <br>";
+echo "Cantidad de palabras de 3 caracter $palabras3 <br>";
+echo "Cantidad de palabras de 4 caracter $palabras4 <br>";
+echo "Cantidad de palabras de más de 4 caracter $palabras0 <br>";
 
 
 ?>
